b07e63527a7889efb2fa2f5fa663a15dba5ae38c
EnayaAme <enaya13ame@gmail.com>
2022-07-29 19:32:29 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments.length === 0 ) {return []} 
+  else { let sorted = matrix.reduce( (array, subArray, index) => {
+   if ( index % 2 === 0 ) { array.push(subArray) }
+   if ( index % 2 !== 0 ) { array.push(subArray.reverse()) }
+   return array;
+   }, []);
+  
+  //let str = sorted.map( subarray => subarray.join('')).join('');
+  //let final = Array.from(str).map( num => +num );
+  
+  return sorted.flat(1);
+  }
+   
 }
